29f46651d6601e98af79245f3f60b3803c101a27
Ikraa <munnisaikra750@gmail.com>
2021-08-15 19:59:52 +0600

    login

---
+++ js/login.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("login-submit").addEventListener('click', function () {
+
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // console.log(userEmail);   
+
+    //set user password:
+    const userField = document.getElementById('user-pass');
+    const userPassword = userField.value;
+
+    if (userEmail == "abc@gmail.com" && userPassword == "123") {
+        window.location.href = "banking.html"
+         
+        // console.log("valid");
+    }
+   
+})
